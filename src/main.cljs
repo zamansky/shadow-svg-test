efc4bdb1442c00c0a23da912551b44df9184b1cf
Mike Zamansky <zamansky@gmail.com>
2020-03-28 20:04:31 -0400

    started svg tests

--- src/main.cljs
+++ src/main.cljs
@@ -4,21 +4,25 @@
             ))
 
 
-
+(defn svg []
+  [:svg {:width "100" :height "100"}
+   [:circle {:cx "50" :cy "50" :r "30" :stroke "green" :stroke-width "4"
+             :fill "yellow"}]
+   ])
 (defn main-component []
   [:div 
    [:h1 "This is a component"]
-   ])
+   (svg)])
 
 
 (defn mount [c]
-  (r/render-component [c] (.getElementById js/document "app"))
-  )
+(r/render-component [c] (.getElementById js/document "app"))
+)
 
 (defn reload! []
-  (mount main-component)
-  (print "Hello reload!"))
+(mount main-component)
+(print "Hello reload!"))
 
 (defn main! []
-  (mount main-component)
-  (print "Hello Main"))
+(mount main-component)
+(print "Hello Main"))
